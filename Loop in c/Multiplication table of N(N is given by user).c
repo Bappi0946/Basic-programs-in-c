645fbb68b4baaafde6f5295b5937062df2562c18
Bappi Rahman <56030194+Bappi0946@users.noreply.github.com>
2019-12-13 20:17:35 +0600

    Create Multiplication table of N(N is given by user).c

---
+++ Loop in c/Multiplication table of N(N is given by user).c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main()
+{    int a,b;
+    printf("Enter the number  you want to that's periodic table: ");
+    scanf("%d",&a);
+    for(b=1;b<=10;b++)
+    printf("%d*%d=%d\n",a,b,a*b);
+}
